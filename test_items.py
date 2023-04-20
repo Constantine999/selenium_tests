1be2086d5ffedff6af89559ecb2d85123718ace6
Konstantin <94008109+Constantine999@users.noreply.github.com>
2023-04-20 14:36:30 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_addBasketButton(browser):
+def test_add_basket_to_button(browser):
     browser.get(link)
     time.sleep(30)
     button = browser.find_elements(By.CSS_SELECTOR, "button.btn.btn-lg.btn-primary.btn-add-to-basket")
